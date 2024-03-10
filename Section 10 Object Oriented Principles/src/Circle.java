93efe7578d1794d33f11e3a70dc9382c39ffe2fd
saipraveenseva <saipraveenseva@gmail.com>
2024-03-10 13:09:54 +0530

    initial version

---
+++ Section 10 Object Oriented Principles/src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    public double radius=10;
+    public double area(){
+
+        return 3.14159265*radius*radius;
+    }
+
+    public double perimeter(){
+        return 2*3.14159265*radius;
+    }
+
+}
